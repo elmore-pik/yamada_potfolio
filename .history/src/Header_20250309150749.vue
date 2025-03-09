149b637393d0d2cd451e7eee914e9494c6a02831
elmore-pik <maimait99@gmail.com>
2025-03-09 15:15:21 +0000

    componentsフォルダを作成

---
+++ .history/src/Header_20250309150749.vue
@@ -0,0 +1,7 @@
+<template>
+    <header>
+    <img src="/yamada.png" alt="太郎のアイコン" width="128" height="128">
+    <h1>山田太郎</h1>
+    <p>Webデザイナー見習いです</p>
+  </header>
+</template>
---
+++ .history/src/components/Header_20250309151215.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+    <img src="/yamada.png" alt="太郎のアイコン" width="128" height="128">
+    <h1>山田太郎</h1>
+    <p>Webデザイナー見習いです</p>
+  </header>
+</template>
+
+<style scoped>
+/* headerのスタイル */
+
+header {
+  background: #eee;
+  text-align: center;
+  padding: 32px 0;
+}
+
+header img {
+  border-radius: 50%;
+}
+
+h1 {
+  margin: 16px 0 0;
+  font-weight: normal;
+  font-size: 24px;
+}
+</style>
