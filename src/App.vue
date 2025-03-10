eb1198710f4e70a49e9c7ae954c71e3620ace0dc
elmore-pik <maimait99@gmail.com>
2025-03-10 00:32:48 +0000

    Header.vueに自己紹介を追加

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,13 @@
 <script setup>
 import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <Header></Header>
-  <main>
-    <h2>WORKS</h2>
-    <section>
-      <img src="/work-1.png" alt="すごい勇者ゲーム" width="400" height="240">
-      <h3>すごい勇者ゲーム</h3>
-      <p>すごい勇者ゲームです。すごい勇者ゲームです。すごい勇者ゲームです。すごい勇者ゲームです。すごい勇者ゲームです。すごい勇者ゲームです。すごい勇者ゲームです。</p>
-    </section>
-    <section>
-      <img src="/work-2.png" alt="すごい宝物ゲーム" width="400" height="240">
-      <h3>すごい宝物ゲーム</h3>
-      <p>すごい宝物ゲームです。すごい宝物ゲームです。すごい宝物ゲームです。すごい宝物ゲームです。すごい宝物ゲームです。すごい宝物ゲームです。すごい宝物ゲームです。</p>
-    </section>
-    <section>
-      <img src="/work-3.png" alt="すごいカードゲーム" width="400" height="240">
-      <h3>すごいカードゲーム</h3>
-      <p>すごいカードゲームです。すごいカードゲームです。すごいカードゲームです。すごいカードゲームです。すごいカードゲームです。すごいカードゲームです。すごいカードゲームです。</p>
-    </section>
-  </main>
-
-  <footer>
-    <small>(c) oninstall.com</small>
-  </footer>
+  <Main></Main>
+  <Footer></Footer>
 </template>
 
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,13 @@
+<script setup>
+const intoro = 'フロントエンドエンジニア見習いです';
+</script>
+
 <template>
   <header>
     <img src="/yamada.png" alt="太郎のアイコン" width="128" height="128">
     <h1>山田太郎</h1>
-    <p>Webデザイナー見習いです</p>
+    <p v-text="intoro"></p>
+    <p>{{ intoro }}</p>
   </header>
 </template>
 
